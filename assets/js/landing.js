88606383af1388c9e334881d319023798ba63f53
yakhyo <yakhyo9696@gmail.com>
2025-11-08 21:51:33 +0900

    chore: Update font-family and some minor changes

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -7,7 +7,48 @@ function checkWindowHeight() {
     }
 }
 
+// Typing animation
+const titles = ["ML Software Engineer", "MLOps Engineer"];
+let titleIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+let typingSpeed = 100;
+
+function typeTitle() {
+    const titleElement = document.querySelector('.title');
+    const currentTitle = titles[titleIndex];
+
+    if (isDeleting) {
+        // Remove characters
+        titleElement.textContent = currentTitle.substring(0, charIndex - 1);
+        charIndex--;
+        typingSpeed = 50;
+    } else {
+        // Add characters
+        titleElement.textContent = currentTitle.substring(0, charIndex + 1);
+        charIndex++;
+        typingSpeed = 100;
+    }
+
+    // Check if word is complete
+    if (!isDeleting && charIndex === currentTitle.length) {
+        // Pause at end of word
+        typingSpeed = 2000;
+        isDeleting = true;
+    } else if (isDeleting && charIndex === 0) {
+        // Move to next word
+        isDeleting = false;
+        titleIndex = (titleIndex + 1) % titles.length;
+        typingSpeed = 500;
+    }
+
+    setTimeout(typeTitle, typingSpeed);
+}
+
 // Run the function on window load and resize
-window.onload = checkWindowHeight;
+window.onload = function() {
+    checkWindowHeight();
+    typeTitle();
+};
 window.onresize = checkWindowHeight;
 
